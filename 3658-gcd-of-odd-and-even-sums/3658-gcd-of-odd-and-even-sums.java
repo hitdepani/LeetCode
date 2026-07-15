71103ec01a000f486dc2f04a01b8736185af32ac
Hit Depani <depanihit09@gmail.com>
2026-07-15 17:53:36 +0530

    Time: 0 ms (100%), Space: 42.7 MB (20.94%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,13 +1,13 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int x=n*n;//odd;
-        int y=n*(n-1);//even
-        for(int i=Math.min(x,y);i>0;i--)
-        {
-            if(x%i==0 &&y%i==0)
-            return i;
-        }
-        return 1;
+        // int x=n*n;//odd;
+        // int y=n*(n-1);//even
+        // for(int i=Math.min(x,y);i>0;i--)
+        // {
+        //     if(x%i==0 &&y%i==0)
+        //     return i;
+        // }
+        return n;
     }
     
 }
