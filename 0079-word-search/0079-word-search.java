e878862aed86436795d13ef0849f66715c16a738
Hit Depani <depanihit09@gmail.com>
2026-07-17 19:06:22 +0530

    Time: 130 ms (74.02%), Space: 42.9 MB (75.75%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -8,39 +8,40 @@ class Solution {
         {
             for(int j=0;j<m;j++)
             {
-                if(board[i][j]==word.charAt(0))
+                if(board[i][j]==word.charAt(0)&&dfs(board,i,j,word,0))
                 {
-                    //boolean[][] visited=new boolean[n][m];
-                    dfs(board,i,j,word,0);
-                    
+                    return true;
                 }
-                if(check)
-                return true;
+                // if(check)
+                // return true;
             }
         }
         return false;
 
     }
-    void dfs(char[][] board,int i,int j,String word,int a)
+    boolean dfs(char[][] board,int i,int j,String word,int a)
     {
         if(a==word.length())
         {
-            check=true;
+            return true;
         }
         if(i<0||i>=board.length||j<0||j>=board[0].length)
-        return;
+        return false;
         
         if(a<word.length() && board[i][j]==word.charAt(a))
         {
             char c=board[i][j];
             board[i][j]='*';
             //visited[i][j]=true;
-            dfs(board,i+1,j,word,a+1);
-            dfs(board,i,j+1,word,a+1);
-            dfs(board,i-1,j,word,a+1);
+           boolean found= dfs(board,i+1,j,word,a+1)||
+            dfs(board,i,j+1,word,a+1)||
+            dfs(board,i-1,j,word,a+1)||
             dfs(board,i,j-1,word,a+1);
             //visited[i][j]=false;
             board[i][j]=c;
+            return found;
         }
+        return false;
+        
     }
 }
