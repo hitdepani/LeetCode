dbc73cc893b2bd2c222d369a7fdace4cbdc99033
Hit Depani <depanihit09@gmail.com>
2026-07-21 19:20:21 +0530

    Time: 82 ms (70.42%), Space: 47.9 MB (64.79%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -4,54 +4,37 @@ class Solution {
         if(n==1)
         return s.charAt(0)-48;
         char[] arr = s.toCharArray();
-        int max=Integer.MIN_VALUE;
+        int max=0;
         int base=0;
         for(int i=0;i<n;i++)
         {
-            if(s.charAt(i)=='1')
+            if(arr[i]=='1')
             base++;
         }
        
-       
+       int pz=0;
         int i=0;
+        while (i<n&&arr[i]=='0') {
+            pz++;
+            i++;
+        }
         while(i<n)
         {
-            if(arr[i]=='1')
-            {
-                int k=i-1;
-                
-                int j=i+1;
-                int newz=0;
-                while(j<n&&arr[j]=='1'){
-                     j++;
-                }
-                
-                int l=j;
-                //lesft
-                while(k>=0&&arr[k]=='0')
-                {
-                    k--;
-                    newz++;
-                }
-                
-                //right
-                while(l<n&&arr[l]=='0')
-                {
-                    l++;
-                    newz++;
-                }
-                if(l!=j&&k!=i-1)
-                {
-                    max=Math.max(max,newz+base);
-                    i=j;
-                }
-                i=j;
-            }
-            else
+            while(i<n&&arr[i]=='1')
             i++;
+            int cz=0;
+            while(i<n&&arr[i]=='0')
+            {
+                cz++;
+                i++;
+            }
+            if(pz>0&&cz>0)
+            max=(Math.max(max,pz+cz));
 
+            pz=cz;
+            
         }
-        return Math.max(base,max);
+        return base+max;
 
     }
 }
