df9f89bf7eb915dd5995f977f8bc6bc9cfba46da
Hit Depani <depanihit09@gmail.com>
2026-07-21 19:24:50 +0530

    Time: 80 ms (70.42%), Space: 48 MB (54.93%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,40 +1,41 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        int n=s.length();
-        if(n==1)
-        return s.charAt(0)-48;
+        int n = s.length();
+        if (n == 1)
+            return s.charAt(0) - 48;
         char[] arr = s.toCharArray();
-        int max=0;
-        int base=0;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]=='1')
-            base++;
+        int max = 0;
+        int base = 0;
+        for (char c : arr) {
+            if (c == '1')
+                base++;
         }
-       
-       int pz=0;
-        int i=0;
-        while (i<n&&arr[i]=='0') {
+
+        int pz = 0;
+        int i = 0;
+        while (i < n && arr[i] == '0') {
             pz++;
             i++;
         }
-        while(i<n)
-        {
-            while(i<n&&arr[i]=='1')
-            i++;
-            int cz=0;
-            while(i<n&&arr[i]=='0')
-            {
+        while (i < n) {
+            while (i < n && arr[i] == '1')
+                i++;
+            int cz = 0;
+            while (i < n && arr[i] == '0') {
                 cz++;
                 i++;
             }
-            if(pz>0&&cz>0)
-            max=(Math.max(max,pz+cz));
-
-            pz=cz;
-            
+            if (pz > 0 && cz > 0)
+                max = (Math.max(max, pz + cz));
+            if (pz > 0 && cz > 0) {
+                int currentGain = pz + cz;
+                if (currentGain > max) {
+                    max = currentGain;
+                }
+            }
+            pz = cz;
         }
-        return base+max;
+        return base + max;
 
     }
 }
