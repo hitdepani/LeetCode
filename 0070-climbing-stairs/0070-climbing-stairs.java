4d35d53a3889e79ae90eb32d37d7b9ff31d5869c
Hit Depani <depanihit09@gmail.com>
2026-07-22 19:21:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,7 @@
 class Solution {
     public int pro(int n,int []arr)
     {
-        if(n<=3)
-        return n;
+        
         if(arr[n]!=0)
         return arr[n];
         arr[n]=pro(n-1,arr)+pro(n-2,arr);
@@ -10,8 +9,16 @@ class Solution {
         
     }
     public int climbStairs(int n) {
-        int arr[]= new int[n+1];
-        return pro(n,arr);
-
-    }
+        if(n<=3)
+        return n;
+        int x=2;
+        int y=3;
+        for(int i=4;i<=n;i++)
+        {
+            int c=x+y;
+            x=y;
+            y=c; 
+        }
+        return y;
+        }
 }
