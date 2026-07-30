7e2ec48f7588eaf84027dad3a6f12a49084d51a8
Hit Depani <depanihit09@gmail.com>
2026-07-30 23:33:48 +0530

    Time: 0 ms (100%), Space: 42.3 MB (29.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,18 @@
 class Solution {
+    int sol(int n,int m,int dp[][])
+    {
+        if(n<0||m<0)
+        return 0;
+        else if(n==0&&m==0)
+        return 1;
+        else if(dp[n][m]!=0)
+        return dp[n][m];
+        else 
+             dp[n][m]=sol(n-1,m,dp)+sol(n,m-1,dp);
+             return dp[n][m];
+        
+
+    }
     public int uniquePaths(int m, int n) {
         long p=1;
         int k=Math.min(m-1,n-1);
