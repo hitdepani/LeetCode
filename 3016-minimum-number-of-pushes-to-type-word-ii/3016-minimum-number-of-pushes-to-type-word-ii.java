251cca3c3c84b963c7a02a748fe1424c0beef6dd
Hit Depani <depanihit09@gmail.com>
2026-07-31 15:07:32 +0530

    Time: 10 ms (66.76%), Space: 48 MB (52.6%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,43 +1,20 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character,Integer> map= new HashMap<>();
-        char[] arr=new char[26];
-        int n=word.length();
-        for(int i=0;i<n;i++)
-        {
-            if(map.containsKey(word.charAt(i)))
-            {
-                map.put(word.charAt(i),map.get(word.charAt(i))+1);
-            }
-            else 
-            map.put(word.charAt(i),1);
+        int[] freq = new int[26];
+        char[] chars = word.toCharArray();
+        for(char c : chars){
+            freq[c - 'a']++;
         }
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
-        Iterator<Map.Entry<Character, Integer>> iterator = list.iterator();
-        int res=0;
-        int x=1;
-        while(iterator.hasNext())
-        {
-            Map.Entry<Character,Integer> e=iterator.next();
-            char c=e.getKey();
-            int cc=e.getValue();
-            if(x<=8)
-            {
-                res+=cc;
-            }
-            else if(x<=16)
-            {
-                res=res+(cc*2);
-            }
-            else if(x<=24)
-            {
-                res=res+(cc*3);
-            }
-            else
-            res=res+(cc*4);
-            x++;
+        Arrays.sort(freq);
+        int cost = 0;
+        int assigned = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+            cost += freq[i] * (assigned / 8 + 1);
+            assigned++;
         }
-        return res;
+
+        return cost;
     }
 }
