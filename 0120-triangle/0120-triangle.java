742aacca1312f1b8eedb431bbb3c4f33a55316bd
Hit Depani <depanihit09@gmail.com>
2026-08-01 12:30:12 +0530

    Time: 3 ms (80.01%), Space: 45.6 MB (40.46%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -21,6 +21,15 @@ class Solution {
             for(int j=0;j<=m;j++)
             dp[i][j]=Integer.MAX_VALUE;
         }
-        return sol(n-1,m-1,dp,triangle,0,0);
+        for(int i=0;i<n;i++)
+        dp[n-1][i]=triangle.get(n-1).get(i);
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                dp[i][j]=Math.min(triangle.get(i).get(j)+dp[i+1][j],triangle.get(i).get(j)+dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 }
