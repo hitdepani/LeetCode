2b6d523639ac61177566cacaa830d5f1b0de1ecc
Hit Depani <depanihit09@gmail.com>
2026-08-01 12:30:32 +0530

    Time: 3 ms (80.01%), Space: 45.7 MB (40.46%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,17 +1,5 @@
 class Solution {
-    int sol(int n,int m,int dp[][],List<List<Integer>> triangle,int i,int j)
-    {
-        
-        if(dp[i][j]!=Integer.MAX_VALUE)
-        return dp[i][j];
-        if(i==n&&j==m)
-        return triangle.get(i).get(j);
-        if(i<0||j<0)
-        return 0;
-        if(i>n||j>=m)
-        return 0;
-        return dp[i][j]=triangle.get(i).get(j)+Math.min(sol(n,m,dp,triangle,i+1,j),sol(n,m,dp,triangle,i+1,j+1));
-    }
+    
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         int m=triangle.get(n-1).size();
