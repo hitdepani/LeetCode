5f05bbde20b8ccfdcc8c4d784d71e453ddbace19
Hit Depani <depanihit09@gmail.com>
2026-08-03 11:47:47 +0530

    Time: 10 ms (83.24%), Space: 85.7 MB (59.37%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,26 +1,32 @@
 class Solution {
-    int sol(int i,int stoneValue[],int n,int dp[])
-    {
-        if(i>=n)
-        return 0;
+    // int sol(int i,int stoneValue[],int n,int dp[])
+    // {
+    //     if(i>=n)
+    //     return 0;
         
-        if(dp[i]!=Integer.MIN_VALUE)
-        return dp[i];
-        int x=0,max=Integer.MIN_VALUE;
-        for(int j=0;j<3&&i+j<n;j++)
-        {
-            x+=stoneValue[j+i];
-            max=Math.max(max,x-sol(i+j+1,stoneValue,n,dp));
-        }
-        return dp[i]=max;
+    //     if(dp[i]!=Integer.MIN_VALUE)
+    //     return dp[i];
         
-    }
+        
+    //     return dp[i]=max;
+        
+    // }
     public String stoneGameIII(int[] stoneValue) {
         int n =stoneValue.length;
         int dp[]= new int[n+1];
-        Arrays.fill(dp,Integer.MIN_VALUE);
-        int x=sol(0,stoneValue,n,dp);
-       // return ""+x;
+        //Arrays.fill(dp,Integer.MIN_VALUE);
+        for(int i=n-1;i>=0;i--)
+        {
+            int y=0;
+            dp[i]=Integer.MIN_VALUE;
+            for(int j=0;j<3&&i+j<n;j++)
+            {
+                y+=stoneValue[j+i];
+                dp[i]=Math.max(dp[i],y-dp[i+j+1]);
+            }
+            //dp[i]=max;
+        }
+        int x=dp[0];
         if(x==0)
         return "Tie";
         else if(x>0)
