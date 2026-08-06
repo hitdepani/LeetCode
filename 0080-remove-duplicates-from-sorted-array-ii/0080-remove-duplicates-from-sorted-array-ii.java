4902422afdf6090ed6de9b2bb2bf20db793c1cc6
Hit Depani <depanihit09@gmail.com>
2026-08-06 12:32:13 +0530

    Time: 0 ms (100%), Space: 48.8 MB (32.71%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int i=0;
         for(int n:nums)
         {
-            if(i<2 ||n>nums[i-2])
+            if(i<2||n>nums[i-2])
             {
                 nums[i]=n;
                 i++;
