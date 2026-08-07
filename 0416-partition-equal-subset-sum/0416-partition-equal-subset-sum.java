3b22630dd0b562253305ec54af9601b9c2a2d221
Hit Depani <depanihit09@gmail.com>
2026-08-07 22:23:28 +0530

    Time: 31 ms (81.78%), Space: 44.1 MB (71.45%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -11,25 +11,15 @@ class Solution {
         if(sum%2==1) return false;
         
         int target=sum/2;
-        boolean dp[][]= new boolean [n][target+1];
-        for (int i=0;i<n;i++) {
-            dp[i][0]=true;
-        }
-        if(nums[0]<=target)
-        dp[0][nums[0]]=true;
-        for(int i=1;i<n;i++)
+        boolean dp[]= new boolean [target+1];
+        dp[0]=true;
+        for(int i:nums)
         {
-            for(int j=1;j<=target;j++)
+            for(int j=target;j>=i;j--)
             {
-                    boolean notake=dp[i-1][j];
-                    boolean take=false;
-                    if(j>=nums[i])
-                    {
-                        take=dp[i-1][j-nums[i]];
-                    }
-                    dp[i][j]=take||notake;
+                    dp[j]=dp[j]||dp[j-i];
             }
         }
-        return dp[n-1][target];
+        return dp[target];
     }
 }
