b594f4f9a94bcf7a62a578847e5c186501e7dec8
Hit Depani <depanihit09@gmail.com>
2026-08-09 18:10:01 +0530

    Time: 264 ms (33.4%), Space: 47.3 MB (7.88%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -2,19 +2,19 @@ class Solution {
     int sol(int nums[],int target,int i,int sum,int dp[][])
     {
         
-        if(sum==target && i==-1)
+        if(sum==target && i==nums.length)
         return 1;
-        if(i<0)
+        if(i>=nums.length)
         return 0 ;
         if(dp[i][sum+1000]!=0)
         return dp[i][sum+1000];
-        return dp[i][sum+1000]=sol(nums,target,i-1,sum-nums[i],dp)+sol(nums,target,i-1,sum+nums[i],dp);
+        return dp[i][sum+1000]=sol(nums,target,i+1,sum-nums[i],dp)+sol(nums,target,i+1,sum+nums[i],dp);
         
         
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
         int dp[][]= new int[n][3000];
-        return sol(nums,target,n-1,0,dp);
+        return sol(nums,target,0,0,dp);
     }
 }
