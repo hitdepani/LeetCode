b83978a2c32f4dff476882b7f8a0310f38051b63
Hit Depani <depanihit09@gmail.com>
2026-08-10 18:17:33 +0530

    Time: 15 ms (64.49%), Space: 42.5 MB (68.41%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,22 +1,21 @@
 class Solution {
-    boolean sol(int n,Boolean dp[])
-    {
-        
-        if(n==0)
-        return false;
-        if(dp[n]!=null)
-        return dp[n];
-        for(int i=1;i*i<=n;i++)
-        {
-            int x=n-(i*i);
-            if(sol(x,dp)==false)
-            return dp[n]=true;
-        }
-        return dp[n]=false;
-
-    }
     public boolean winnerSquareGame(int n) {
-       Boolean dp[]= new Boolean[n+1];
-       return sol(n,dp);
+       boolean dp[]= new boolean[n+1];
+       
+       for(int i=1;i<=n;i++)
+       {
+        for(int j=1;j*j<=i;j++)
+        {
+            int x=i-(j*j);
+            if(dp[x]==false)
+            {
+                dp[i]=true;
+                break;
+            }
+        }
+            
+            
+       }
+       return dp[n];
     }
 }
