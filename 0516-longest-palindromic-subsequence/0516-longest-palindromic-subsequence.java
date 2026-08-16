96979bc7c6825dd18289a203568b7f60e7caf7ee
Hit Depani <depanihit09@gmail.com>
2026-08-16 12:46:00 +0530

    Time: 57 ms (40.47%), Space: 63.6 MB (70.6%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,22 +1,13 @@
 class Solution {
     
     public int longestPalindromeSubseq(String s) {
-        StringBuilder ss=new StringBuilder(s);
-        ss.reverse();
-        int dp[][]= new int[s.length()+1][ss.length()+1];
-        // for(int i=0;i<s.length();i++)
-        // {
-        //     dp[i][0]=0;
-        // }
-        // for(int i=0;i<ss.length();i++)
-        // {
-        //     dp[0][i]=0;
-        // }
-        for(int i=1;i<=s.length();i++)
+        int n=s.length();
+        int dp[][]= new int[n+1][n+1];
+        for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=ss.length();j++)
+            for(int j=1;j<=n;j++)
             {
-                if(s.charAt(i-1)==ss.charAt(j-1))
+                if(s.charAt(i-1)==s.charAt(n-j))
                 {
                    dp[i][j]=1+dp[i-1][j-1];
                 }
@@ -26,6 +17,6 @@ class Solution {
                 }
             }
         }
-        return dp[s.length()][ss.length()];
+        return dp[n][n];
     }
 }
